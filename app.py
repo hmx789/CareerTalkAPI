18de1a5fa58f1d67946fb17ed4c23ee6066c5990
Seho Lim <limseho657424@gmail.com>
2018-08-26 21:45:24 -0500

    Finished the function that parses google sheet to get data.

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
 from flask import Flask
+from sqlalchemy import create_engine, asc, desc
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, User, Company, Fair
+
 from googleapiclient.discovery import build
 from httplib2 import Http
 from oauth2client import file, client, tools
 import json
 
 app = Flask(__name__)
+
 with open('config.json', 'r') as f:
     config = json.load(f)
 
+postgres = config["POSTGRES"]
+engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(
+                                                postgres["user"],
+                                                postgres["pw"],
+                                                postgres["endpoint"],
+                                                postgres["port"],
+                                                postgres["db"]))
 
-SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
-SPREADSHEET_ID = '1fKG4iVnj9coxg2mwip4reD7Rt5eiBvlEDM-Hu84M3zE'
-RANGE_NAME = 'Sheet1!A4:E48'
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+
+session = DBSession()
 
 def get_company_info():
+    SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+    SPREADSHEET_ID = '1fKG4iVnj9coxg2mwip4reD7Rt5eiBvlEDM-Hu84M3zE'
+    RANGE_NAME = 'Sheet1!A4:E48'
     store = file.Storage('token.json')
     creds = store.get()
     if not creds or creds.invalid:
@@ -26,21 +42,76 @@ def get_company_info():
                                                  range=RANGE_NAME).execute()
     values = result.get('values', [])
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print(row)
-
     return values
+
+
+def insert_rows():
+    """
+    :return:
+    """
+    data = get_company_info()
+
+    for row in data:
+        name = row[0]
+
+        if row[1].strip().lower() == 'int':
+            type = 1
+        elif row[1].strip().lower() == 'ft':
+            type = 2
+        else :
+            type = 3
+
+        if row[3].strip().lower() == 'bs':
+            degree = 1
+        elif row[3].strip().lower() == 'ms':
+            degree = 2
+        elif row[3].strip().lower() == 'phd':
+            degree = 3
+        elif row[3].strip().lower() == 'bs, ms':
+            degree = 4
+        elif row[3].strip().lower() == 'bs, phd':
+            degree = 5
+        elif row[3].strip().lower() == 'ms, phd':
+            degree = 6
+        else:
+            degree = 8
+
+        if row[4].strip().lower() == 'yes':
+            visa = 1
+        elif row[4].strip().lower() == 'no':
+            visa = 2
+        else:
+            vias = 3
+
+        print("name: {}, hiringtype: {}, degree: {}, visa: {}".format(name,
+                                                                      type,
+                                                                      degree,
+                                                                      visa))
+        print(row[2])
+        print("Adding a company . . .")
+
+        '''
+        id = Column(Integer, primary_key=True)
+        name = Column(String(100), nullable=False)
+        description = Column(String(), nullable=False)
+        hiring_types = Column(Integer, ForeignKey('hiring_type.id'))
+        hiring_majors = Column(String())
+        degree = Column(Integer, ForeignKey('degree_type.id'))
+        visa = Column(Integer, ForeignKey('visa_type.id'))
+        fair_id = Column(Integer, ForeignKey('fair.id'))
+        fair = relationship(Fair)
+
+        '''
+        company = Company(name=name, hiring_types=type, hiring_majors=row[2],
+                          degree=degree, visa=visa, fair_id=1)
+        session.add(company)
+        session.commit()
+
 
 @app.route('/')
 @app.route('/main')
 def main():
     return "hello world"
-
 
 if __name__ == "__main__":
     app.secret_key = config['DEFAULT']['SECRET_KEY']
