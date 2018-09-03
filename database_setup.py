364a17a8f318e7e66da82be3d2a2efb33515250e
Seho Lim <limseho657424@gmail.com>
2018-09-03 14:12:17 -0500

    Added some helper functions and callback for the social login.

--- database_setup.py
+++ database_setup.py
@@ -3,15 +3,15 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Date, Time
 from sqlalchemy.orm import relationship
 import json
-import os,sys, inspect
+import os, sys, inspect
 
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 with open('{}/config.json'.format(current_dir), 'r') as f:
     config = json.load(f)
+
 postgres = config["POSTGRES"]
 Base = declarative_base()
-
 
 
 def _to_minutes(time):
@@ -22,12 +22,13 @@ def _to_minutes(time):
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    kind = Column(Integer)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+    kind = Column(Integer)
     picture = Column(String(250))
     resume = Column(String(250))
     bio = Column(String(250))
+
 
     def serialize(self):
         type = ['student','faculty', 'corporate']
@@ -78,6 +79,51 @@ class Fair(Base):
         }
 
 
+class Company(Base):
+    __tablename__ = 'company'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    description = Column(String())
+    hiring_types = Column(Integer, ForeignKey('hiring_type.id'))
+    hiring_majors = Column(String())
+    degree = Column(Integer, ForeignKey('degree_type.id'))
+    visa = Column(Integer, ForeignKey('visa_type.id'))
+    fair_id = Column(Integer, ForeignKey('fair.id'))
+    company_url = Column(String())
+    fair = relationship('Fair')
+    fovorite = relationship('Favorite')
+
+    @property
+    def serialize(self):
+        degree = [['BS'], ['MS'], ['PhD'], ['BS', 'MS'], ['BS', 'PhD'],
+                  ['MS', 'PhD'], ['BS', 'MS', 'PhD']]
+        types = [['INT'], ['FT'], ['INT', 'FT']]
+        visa = ['yes', 'no', 'maybe']
+        majors = [major.strip() for major in self.hiring_majors.split(',')]
+
+
+        return {
+            'fair': self.fair.name,
+            'fair_id': self.fair_id,
+            'visa': visa[self.visa-1],
+            'degree': degree[self.degree-1],
+            'hiring_majors': majors,
+            'hiring_types': types[self.hiring_types-1],
+            'description': self.description,
+            'company_url': self.company_url,
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Favorite(Base):
+    __tablename__ = 'favorite_company'
+    id = Column(Integer, primary_key=True)
+    company_id = Column(Integer, ForeignKey('company_id'))
+    user_id = Column(Integer, ForeignKey('user_id'))
+    fair_id = Column(Integer, ForeignKey('fair_id'))
+
+
 class HiringType(Base):
     __tablename__ = 'hiring_type'
     id = Column(Integer, primary_key=True)
@@ -96,63 +142,6 @@ class Visa(Base):
     type = Column(String(6), nullable=False)
 
 
-class Company(Base):
-    __tablename__ = 'company'
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
-    description = Column(String())
-    hiring_types = Column(Integer, ForeignKey('hiring_type.id'))
-    hiring_majors = Column(String())
-    degree = Column(Integer, ForeignKey('degree_type.id'))
-    visa = Column(Integer, ForeignKey('visa_type.id'))
-    fair_id = Column(Integer, ForeignKey('fair.id'))
-    company_url = Column(String())
-    fair = relationship('Fair')
-
-    @property
-    def serialize(self):
-        """
-        Return object data in easily serializable format
-
-        hiring types:
-        1: intern
-        2: full time
-        3: intern + full time
-
-        degree:
-        1: BS
-        2: MS
-        3: PhD
-        4: BS, MS
-        5: BS, PhD
-        6: MS, PhD
-        7: BS, MS, PhD
-
-        visa:
-        1: false
-        2: true
-        3: maybe
-        """
-
-        degree = [['BS'], ['MS'], ['PhD'], ['BS', 'MS'], ['BS', 'PhD'],
-                  ['MS', 'PhD'], ['BS', 'MS', 'PhD']]
-
-        types = [['INT'], ['FT'], ['INT', 'FT']]
-        visa = ['yes', 'no', 'maybe']
-        majors = [major.strip() for major in self.hiring_majors.split(',')]
-
-        return {
-            'fair': self.fair.name,
-            'fair_id': self.fair_id,
-            'visa': visa[self.visa-1],
-            'degree': degree[self.degree-1],
-            'hiring_majors': majors,
-            'hiring_types': types[self.hiring_types-1],
-            'description': self.description,
-            'company_url': self.company_url,
-            'name': self.name,
-            'id': self.id,
-        }
 """
 engine = create_engine('postgresql://{}:{}@{}:{}/{}'.format(
                                                 postgres["user"],
